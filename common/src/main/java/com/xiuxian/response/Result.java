0f826c3e17a97787250b011279c9ca745139f574
zhy <2390318635@qq.com>
2022-06-14 00:49:38 +0800

    add: 项目搭建：logback配置 & 全局对象封装 & 全局异常处理

---
+++ common/src/main/java/com/xiuxian/response/Result.java
@@ -0,0 +1,35 @@
+package com.xiuxian.response;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * @author zhy
+ * @date 2022/6/13 22:51
+ * @description Result
+ */
+@Data
+@Accessors(chain = true)
+public final class Result<T> {
+    private Boolean success;
+    private Integer code;
+    private String msg;
+    private String responseId;
+    private T data;
+
+    public static <T> Result<T> ofSuccess(T data) {
+        return new Result<T>().setSuccess(true).setCode(200).setMsg("成功").setData(data);
+    }
+
+    public static Result<Void> ofFailure(Integer errCode, String errorMsg) {
+        return new Result<Void>().setSuccess(false).setCode(errCode).setMsg(errorMsg);
+    }
+
+    public static void main(String[] args) {
+        Result<String> stringResult = ofSuccess("测试一哈~");
+        Result<Void> voidResult = ofFailure(1000, "失败了~");
+        System.out.println("=== ：" + JSON.toJSONString(voidResult));
+    }
+}
