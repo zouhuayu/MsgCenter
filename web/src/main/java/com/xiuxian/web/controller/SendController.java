7c8213dd3d3d6ac991d00a6c74cb537926d80c02
zhy <2390318635@qq.com>
2022-06-14 03:30:08 +0800

    add: 项目搭建：接入redis

--- web/src/main/java/com/xiuxian/web/controller/SendController.java
+++ web/src/main/java/com/xiuxian/web/controller/SendController.java
@@ -5,12 +5,19 @@ import com.xiuxian.exception.ValidParamException;
 import com.xiuxian.response.Result;
 import com.xiuxian.web.request.BaseReq;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class SendController {
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @RequestMapping("/send")
     public Result<String> send(@RequestBody BaseReq baseReq) {
@@ -18,7 +25,15 @@ public class SendController {
         if (StringUtils.isBlank(baseReq.getUserName())) {
             throw new ValidParamException(ExceptionEnum.ERROR_PARAM);
         }
-        return Result.ofSuccess("信息发送成功");
+
+        // 操作Redis中的string类型的数据,先获取ValueOperation
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+
+        // 添加数据到redis
+        valueOperations.set("msgName", "xxxx");
+
+
+        return Result.ofSuccess("信息发送成功" + valueOperations.get("msgName"));
     }
 
 }
